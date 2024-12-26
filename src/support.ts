1be1b030ce01ff20c8c6ddda09c8510dc6258e31
legendtool <legendtools@hotmail.com>
2024-12-27 00:01:02 +0800

    修改in18

--- src/support.ts
+++ src/support.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
-import Support from './Support.vue'
 import i18n from './i18n'
+import Support from './Support.vue'
 
 const app = createApp(Support)
 app.use(i18n)
