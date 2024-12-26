81566819b7bae8012bb12c4d3bfe5cd3fb07c413
legendtool <legendtools@hotmail.com>
2024-12-26 23:54:25 +0800

    再次尝试

--- src/support.ts
+++ src/support.ts
@@ -1,10 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
-import Support from './pages/Support.vue'
+import Support from './Support.vue'
 import i18n from './i18n'
-// import router from './router'
 
 const app = createApp(Support)
 app.use(i18n)
-// app.use(router)
 app.mount('#app')
